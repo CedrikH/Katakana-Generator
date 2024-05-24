db0c74cd068b8fa716722abefcc5dd0d7fb610c4
CedrikH <49614541+CedrikH@users.noreply.github.com>
2024-05-24 17:30:35 +0200

    Filtered out rarely used Katakana

--- main.py
+++ main.py
@@ -2,26 +2,17 @@ import random
 import unicodedata
 
 def main():
-    def get_all_katakana():
-        katakana_list = []
-        for code_point in range(0x30A0, 0x30FF + 1):
-            char = chr(code_point)
-            if unicodedata.name(char, '').startswith('KATAKANA'):
-                katakana_list.append(char)
-        return katakana_list
+    katakana_list = [chr(code) for code in range(0x30A0, 0x30FB) if code not in [0x30F7, 0x30F8, 0x30F9, 0x30FA, 0x30F5, 0x30F6, 0x30FB, 0x30F0, 0x30A0]]
 
-# Example usage:
-    katakana_chars = get_all_katakana()
     n = int(input("Enter the number of katakana characters to be generated: "))
     result = []
-    for number in range (n):
-        result.append(random.choice(katakana_chars))
+    for _ in range(n):
+        result.append(random.choice(katakana_list))
 
     katakana_string = " ".join(result)
 
     with open('katakana_output.txt', 'w', encoding='utf-8') as file:
         file.write(katakana_string)
-
 
 if __name__ == "__main__":
     main()
